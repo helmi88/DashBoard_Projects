8e51c65a515e6ad0cde5c00f54e911643b532c49
Boubaker Helmi <helmiboubaker@gmail.com>
2024-07-31 10:56:01 +0100

    add item component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,32 @@
+import './todolist.css';
+import Item from './Todolist/Item';
+
 function TodoList() {
-    return <div>
-        <h1>container for the project Todo Lists</h1>
+    return (
+    <div className='todo-container'>
+            <h1 className='todo-entete'>Container for the project Todo Lists</h1>
+            <div className='todo-body'>
+                <div className='todo-section'>
+                    <h3>Thinks to do</h3>
+                    <div className='todo-items'>
+                        <Item />
+                    </div>
+                </div>
+                <div className='todo-section'>
+                    <h3>Thinks in progress</h3>
+                    <div className='todo-items'>
+                        <Item />
+                    </div>
+                </div>
+                <div className='todo-section'> 
+                    <h3>Finished thinks</h3>
+                    <div className='todo-items'>
+                        <Item />
+                    </div>
+                </div>
+            </div>
     </div>
+    )
 }
 
 export default TodoList;
